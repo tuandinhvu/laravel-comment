9a4e6de64090a60aceb6248461fe0c36211d3843
Tuấn Đinh <tuan3162@gmail.com>
2019-02-28 12:31:26 +0700

    fix

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -4,11 +4,13 @@ namespace Fastup\Comment\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Menu;
+use App\RealEstate;
 use App\Services\ProvinceService;
 use Carbon\Carbon;
 use Fastup\Comment\Comment;
 use Illuminate\Http\Request;
-use App\RealEstate;
+use Illuminate\Support\Facades\Log;
+
 class CommentController extends Controller
 {
     public function create(){
@@ -23,9 +25,11 @@ class CommentController extends Controller
         $comment->created_at    =   Carbon::now();
         $comment->save();
         if(function_exists('notify')){
+            Log::info('zzzzz');
             switch ($comment->type){
                 case 'realestate':
                     $user_id    =   ($realestate = RealEstate::find($comment->source_id))?RealEstate::find($comment->source_id)->posted_by:auth()->user()->id;
+                    Log::info($user_id .'-'. auth()->user()->id);
                     if($user_id != auth()->user()->id)
                         notify([$user_id], 'Bình luận mới', (auth()->user()->userinfo?auth()->user()->userinfo->full_name:auth()->user()->name).' vừa bình luận về bài viết của bạn.', route('detail-real-estate', ['slug'=>$realestate->slug.'-'.$realestate->id]));
                     break;
